a5b1a3c4eb4ed03496687968336c2f301cb26ec5
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2023-05-31 01:45:34 +0530

    first commit

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format(template, name);
+	}
+}
